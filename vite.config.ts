647e52c7004e68511954d6965cb13563a88fa07e
Sovidi <lim132445@gmail.com>
2025-02-17 21:04:16 +0900

    depth 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import svgrPlugin from 'vite-plugin-svgr';
 
 export default defineConfig({
-  base: '/',
+  base: './',
   plugins: [react(), svgrPlugin()],
   resolve: {
     alias: [
