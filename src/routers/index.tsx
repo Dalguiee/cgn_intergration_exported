a83517d80bb9eb0d5a5a1c5f677ee2bce8b1be81
Sovidi <lim132445@gmail.com>
2025-02-18 18:31:56 +0900

    개발중 표시

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -13,6 +13,22 @@ export default function MainRouter() {
   return (
     <Routes>
       <Route path='/*' element={<DefaultLayer />}>
+        <Route
+          path='tidings/campaign'
+          element={<TidingsCampaignPage />}
+        ></Route>
+        <Route
+          path='tidings/:subDepth/detail'
+          element={<TidingsDetailPage />}
+        ></Route>
+      </Route>
+    </Routes>
+  );
+
+  // 개발중개발중개발중개발중개발중개발중개발중개발중개발중개발중개발중개발중개발중개발중개발중개발중개발중개발중개발중개발중개발중개발중개발중개발중개발중개발중개발중개발중개발중개발중개발중개발중개발중개발중
+  return (
+    <Routes>
+      <Route path='/*' element={<DefaultLayer />}>
         <Route path='home' element={<Home />}></Route>
         <Route
           path='tidings/campaign'
