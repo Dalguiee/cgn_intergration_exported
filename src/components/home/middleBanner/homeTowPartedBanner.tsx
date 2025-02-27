57125e725b87f1ef27ec2749bc6578f68564036f
Sovidi <lim132445@gmail.com>
2025-02-27 21:53:04 +0900

    배너 위치조정

--- src/components/home/middleBanner/homeTowPartedBanner.tsx
+++ src/components/home/middleBanner/homeTowPartedBanner.tsx
@@ -6,9 +6,9 @@ import { homeTowPartedBannersData } from '@/db/mockup';
 
 const HomeTowPartedBanner = ({ mobile }) => {
   return (
-    <div className='flex max-h-200 w-full items-center justify-start pl-16 max-lg:max-h-92'>
+    <div className='flex max-h-200 w-full items-center justify-start max-lg:max-h-92 max-lg:pl-16 lg:px-16'>
       <div
-        className={`flex aspect-[1560/200] h-full w-full max-w-1560 items-center justify-start gap-24 scrollbar-hide max-lg:h-92 max-lg:w-fit max-lg:justify-start max-lg:gap-12 max-lg:overflow-x-scroll`}
+        className={`flex aspect-[1560/200] h-full w-full max-w-1560 items-center justify-start gap-24 scrollbar-hide max-lg:h-92 max-lg:w-fit max-lg:justify-start max-lg:gap-12 max-lg:overflow-x-scroll lg:mx-auto`}
       >
         {homeTowPartedBannersData?.slice(0, 2)?.map((item, key) => (
           <button
