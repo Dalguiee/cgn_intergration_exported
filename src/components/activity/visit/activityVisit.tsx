b137ea30f04af4686aca5ddd415adb98de0b1658
Sovidi <lim132445@gmail.com>
2025-03-13 19:02:22 +0900

    코드병합 완료

--- src/components/activity/visit/activityVisit.tsx
+++ src/components/activity/visit/activityVisit.tsx
@@ -31,7 +31,7 @@ const oddDatas = [
     reverse: true,
     src: `${import.meta.env.VITE_PUBLIC_URL}images/activity/visit_content_3.png`,
     title: `견학 신청`,
-    subTitle: `문의) 커뮤니케이션실 cgnpr@cgnmail.net 02)3275-9321`,
+    subTitle: `문의) 커뮤니케이션실<br className="lg:hidden"/>cgnpr@cgnmail.net 02)3275-9321`,
     text: `CGN의 미디어 선교 사역 현장을 직접 방문해 보세요.<br />제작 스튜디오, 본사 라운딩 등 다양한 체험을 통해 CGN의<br />미디어 선교 사역을 배우고 느끼는 시간을 가질 수 있습니다.<br/ >방문 일자, 인원, 참여 연령을 알려주시면 맞춤형 견학을<br />진행해드립니다.`,
     moText1: `CGN의 미디어 선교 사역 현장을 직접 방문해 보세요.`,
     moText2: `제작 스튜디오, 본사 라운딩 등 다양한 체험을 통해 CGN의 미디어<br />선교 사역을 배우고 느끼는 시간을 가질 수 있습니다.<br/ >방문 일자, 인원, 참여 연령을 알려주시면 맞춤형 견학을<br />진행해드립니다.`,
