9eb60094ffd9f2b36cdf083cb6b205bd3ec4d981
Sovidi <lim132445@gmail.com>
2025-07-20 01:34:34 +0900

    vite url set

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename={'/'}>
+  <BrowserRouter basename={'/cgn_intergration_exported'}>
     <App />
   </BrowserRouter>
 );
