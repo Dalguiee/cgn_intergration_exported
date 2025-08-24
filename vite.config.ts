4fe091001a3a1e658611df7b28c342c753c50e91
Sovidi <lim132445@gmail.com>
2025-08-25 04:02:26 +0900

    vite config 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import svgrPlugin from 'vite-plugin-svgr';
 
 export default defineConfig({
-  base: '/cgn_intergration_exported/',
+  base: '/',
   plugins: [react(), svgrPlugin()],
   resolve: {
     alias: [
